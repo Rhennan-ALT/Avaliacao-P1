8fd9e3aaaf08af5b43262673cfa06874ff672ed4
Rhennan <reehsecundario@gmail.com>
2026-04-13 14:44:38 -0300

    add: rotas

---
+++ app/routers/aluno_rout.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from bson import ObjectId
+from database import alunos_colletion
+from app.schemas.aluno_sch import *
+from app.services.aluno_serv import novo_aluno_service
+
+router = APIRouter(tags=["Insert de Alunos"])
+
+@router.post("/aluno")
+def novo_aluno(aluno: Aluno):
+    return novo_aluno_service(aluno)
